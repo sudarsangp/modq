8811d78ca4c9d016ce888a2ffbdf4c0e8f94c945
sudarsangp <sudarsangp@gmail.com>
2014-07-04 01:35:57 +0800

    Added the entire project

---
+++ ModQ/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ModQ.Startup))]
+namespace ModQ
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
