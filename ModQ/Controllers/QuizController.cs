899690312e5a7d5458e773ca6ea407b39bb195f3
sudarsangp <sudarsangp@gmail.com>
2014-07-04 01:31:48 +0800

    basic side bar, navigation bar, authorization and landing page before and after login done

---
+++ ModQ/Controllers/QuizController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModQ.Controllers
+{
+    [Authorize]
+    public class QuizController : Controller
+    {
+        // GET: Quiz
+        public ActionResult LoginLanding()
+        {
+            return View();
+        }
+    }
+}
