575c0919cbc0a92216f9e90e4b74ea56bfaadae9
sudarsangp <sudarsangp@gmail.com>
2014-12-02 09:36:41 +0800

    version 1 : working version of module quiz web application.

--- ModQ/Content/customJS/quizChoice.js
+++ ModQ/Content/customJS/quizChoice.js
@@ -1,137 +1,101 @@
 ﻿$(function() {
     var quit = false;
-    $('div.options').on('click', function() {
-            if (quit) {
-                return;
-            }
-
-            var thisObject = $(this);
-        console.log(thisObject.children().children().children());
-            thisObject.children().children().children().attr('checked', true);
-            $.post("AnswerCheck", { question: $('#questionPanel').html(), optionValue: thisObject.find('span').html() }, function() {
-
-                })
-                .success(function(data) {
-
-                    $('#answerDescription').html(data.AnswerDescription);
-                    if (data.CorrectChoice) {
-                        thisObject.addClass('panel-success');
-
-                    } else {
-                        thisObject.addClass('panel-danger');
-
-                    }
-
-                })
-                .fail(function() {
-                    alert("error");
-                })
-                .always(function() {
-                    quit = true;
-                });
-              
-       });
-    
-    var quizByTypeText = "faculty", quizByTypeValue = "Engineering";
-    $('.quizByFacultyLink').on('click', function () {
-        quizByTypeText = "faculty";
-        quizByTypeValue = $(this).html();
-
-        $.post("AjaxNextQuestion", { question: $('#questionPanel').html(), quizByTypeText: quizByTypeText, quizByTypeValue: quizByTypeValue }, function () {
-            $('div.options').children().children().children().attr('checked', false);
-            $('div.options').removeClass('panel-success');
-            $('div.options').removeClass('panel-danger');
-            $('#answerDescription').html("");
-            quit = false;
-        })
-            .success(function (data) {
-                $('#facultyType').html(data.Faculty);
-                $('#moduleCode').html(data.Module);
-                $('#questionPanel').html(data.Question);
-                $('#optionOnePanel').html(data.FirstOption);
-                $('#optionTwoPanel').html(data.SecondOption);
-                $('#optionThreePanel').html(data.ThirdOption);
-                $('#optionFourPanel').html(data.FourthOption);
+    var quizByTypeText = 'faculty';
+    var quizByTypeValue = 'Computing';
+    $('div.options').on('click',function () {
+        if (quit) {
+            return;
+        }
+        var thisObject = $(this);
+        thisObject.children().children().children().attr('checked', true);
+        var panelDetails = thisObject.children().children();
+    //    console.log($(panelDetails).find('span'));
+        $.post("AnswerCheck", {question: $("#questionPanel").html(), optionValue: $(panelDetails).find('span').html()}, function() {
+                
             })
-            .fail(function () {
-                alert('error');
-            });
+            .success(function(data) {
+                
+                $('#answerDescription').html(data.AnswerDescription);
+                if (data.CorrectChoice) {
+                    thisObject.addClass('panel-success');
 
-    });
-    $('.quizByModuleLink').on('click', function() {
-        quizByTypeText = "module";
-        quizByTypeValue = $(this).html();
+                } else {
+                    thisObject.addClass('panel-danger');
 
-        $.post("AjaxPreviousQuestion", { question: $('#questionPanel').html(), quizByTypeText: quizByTypeText, quizByTypeValue: quizByTypeValue }, function () {
-            $('div.options').children().children().children().attr('checked', false);
-            $('div.options').removeClass('panel-success');
-            $('div.options').removeClass('panel-danger');
-            $('#answerDescription').html("");
-            quit = false;
-        })
-          .success(function (data) {
-              $('#facultyType').html(data.Faculty);
-              $('#moduleCode').html(data.Module);
-              $('#questionPanel').html(data.Question);
-              $('#optionOnePanel').html(data.FirstOption);
-              $('#optionTwoPanel').html(data.SecondOption);
-              $('#optionThreePanel').html(data.ThirdOption);
-              $('#optionFourPanel').html(data.FourthOption);
-          })
-          .fail(function () {
-              alert('error');
-          });
+                }
 
-    });
-
-    $('#ajaxNext').on('click', function () {
-        
-        $.post("AjaxNextQuestion", { question: $('#questionPanel').html(), quizByTypeText: quizByTypeText, quizByTypeValue: quizByTypeValue }, function () {
-            $('div.options').children().children().children().attr('checked', false);
-            $('div.options').removeClass('panel-success');
-            $('div.options').removeClass('panel-danger');
-            $('#answerDescription').html("");
-            quit = false;
-            })
-            .success(function (data) {
-                $('#facultyType').html(data.Faculty);
-                $('#moduleCode').html(data.Module);
-                $('#questionPanel').html(data.Question);
-                $('#optionOnePanel').html(data.FirstOption);
-                $('#optionTwoPanel').html(data.SecondOption);
-                $('#optionThreePanel').html(data.ThirdOption);
-                $('#optionFourPanel').html(data.FourthOption);
             })
             .fail(function() {
-                alert('error');
-            });
-
-
-    });
-    
-    $('#ajaxPrev').on('click', function () {
-
-        $.post("AjaxPreviousQuestion", { question: $('#questionPanel').html(), quizByTypeText: quizByTypeText, quizByTypeValue: quizByTypeValue }, function () {
-            $('div.options').children().children().children().attr('checked', false);
-            $('div.options').removeClass('panel-success');
-            $('div.options').removeClass('panel-danger');
-            $('#answerDescription').html("");
-            quit = false;
-        })
-            .success(function (data) {
-                $('#facultyType').html(data.Faculty);
-                $('#moduleCode').html(data.Module);
-                $('#questionPanel').html(data.Question);
-                $('#optionOnePanel').html(data.FirstOption);
-                $('#optionTwoPanel').html(data.SecondOption);
-                $('#optionThreePanel').html(data.ThirdOption);
-                $('#optionFourPanel').html(data.FourthOption);
+                alert("error");
             })
-            .fail(function () {
-                alert('error');
-            });
-
-
+            .always(function() {
+            quit = true;
+        });
     });
+
+    $('#ajaxPrev').on('click', function() {
+        $.post("AjaxPreviousQuestion", { question: $("#questionPanel").html(), quizByTypeText: quizByTypeText, quizByTypeValue: quizByTypeValue }, function () {
+
+        })
+           .success(function (data) {
+               $("#facultyType").html(data.Faculty);
+               $("#moduleCode").html(data.Module);
+               $("#questionPanel").html(data.Question);
+               $("#optionOnePanel").html(data.FirstOption);
+               $("#optionTwoPanel").html(data.SecondOption);
+               $("#optionThreePanel").html(data.ThirdOption);
+               $("#optionFourPanel").html(data.FourthOption);
+               $("div.options").removeClass('panel-success');
+               $("div.options").removeClass('panel-danger');
+               $("div.options").children().children().children().removeAttr('checked');
+               $("#answerDescription").html("");
+               quit = false;
+
+           });
+    });
+
+    $('#ajaxNext').on('click', function() {
+        $.post("AjaxNextQuestion", { question: $("#questionPanel").html(), quizByTypeText: quizByTypeText, quizByTypeValue: quizByTypeValue }, function() {
+
+            })
+            .success(function (data) {
+                $("#facultyType").html(data.Faculty);
+                $("#moduleCode").html(data.Module);
+            $("#questionPanel").html(data.Question);
+            $("#optionOnePanel").html(data.FirstOption);
+            $("#optionTwoPanel").html(data.SecondOption);
+            $("#optionThreePanel").html(data.ThirdOption);
+            $("#optionFourPanel").html(data.FourthOption);
+            $("div.options").removeClass('panel-success');
+            $("div.options").removeClass('panel-danger');
+            $("div.options").children().children().children().removeAttr('checked');
+            $("#answerDescription").html("");
+            quit = false;
+           
+        });
+    });
+
+    $("a.quizByFacultyLink").on('click', function () {
+        quizByTypeText = 'faculty';
+        quizByTypeValue = $(this).html();
+    });
+
+    $("a.quizByModuleLink").on('click', function () {
+        quizByTypeText = 'module';
+        quizByTypeValue = $(this).html();
+    });
+
+    //$('a.quizByFaculty').on('click', function() {
+    //    console.log($(this).html());
+    //    $.post("QuizByFaculty", function() {
+
+    //        })
+    //        .success(function() {
+    //            alert("success");
+    //        })
+    //        .fail(function() {
+    //        alert("quizbyfaculty error");
+    //    });
+    //});
 
 });
